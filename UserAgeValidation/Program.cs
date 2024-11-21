12956e98283f86cf676d03afc5d8869807e6168e
karinavainola124 <karina.vainola@gmail.com>
2024-11-22 01:11:17 +0200

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakendus küsib kasutajal valida tema sugu (m/f)
+//rakendus küsib kasutajal sisestada tema perekonnanime
+//lähtudes kasutaja valikust, rakendus tervitab kasutajat järgmiselt:
+//"Welcome, Mr. [kasutaja perekonnanimi] / "Welcome, Ms. [kasutaja perekonnanimi]"
+
+Console.WriteLine("Please, select your gender (m/f):");
+
+char userGender = Char.Parse(Console.ReadLine()); //loeb konsoolist maha andmeid string (sõne) formaadis
+Console.WriteLine("Please, enter your last name:");
+string userLastname = Console.ReadLine();
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {userLastname}!");
+}
+else if (userGender == 'f') 
+{
+    Console.WriteLine($"Welcome, Ms. {userLastname}!");
+}
+else 
+{
+    Console.WriteLine($"Welcome, {userLastname}!");
+}
